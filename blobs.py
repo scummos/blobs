bab97672a54706c2f1f8fa1e36b156d45c08993e
Sven Brauch <mail@svenbrauch.de>
2016-05-26 22:59:33 +0200

    initial commit

---
+++ blobs.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+class User:
+    def __init__(self, userid, connection):
+        pass
+
+    @staticmethod
+    def login(username, password):
+        return User()
+
+    @staticmethod
+    def register(username, password):
+        pass
+
+    def sendMessage(self, message):
+        pass
+
+    def receiveTurnCommand(self):
+        pass
+
+    def askTurn(self, match: Match):
+        self.sendMessage(match.stateString())
+        return self.receiveTurnCommand()
+
+class Lobby:
+    def __init__(self):
+        pass
+
+    def joinUser(self, user: User):
+        pass
+
+    def _match(self) -> Match:
+        pass
+
+    def waitForPlayers(self):
+        pass
+
+class Turn:
+    def __init__(self, source, dest):
+        pass
+
+class Match:
+    def __init__(self, users, board):
+        pass
+
+    def execTurn(self, turn):
+        pass
+
+    def turn(self):
+        for user in self.users:
+            turn = user.askTurn(self)
+            self.execTurn(turn)
+            self.checkConnectivity(turn, user)
+            self.checkMatchFinished()
+
+    def checkConnectivity(self, turn, user):
+        pass
+
+    def checkMatchFinished(self):
+        pass
+
+class Board:
+    def __init__(self, size):
+        self.values = np.zeros((size, size), dtype=np.uint16)
+        self.owner = np.zeros_like(self.values)
+
+    def _connected(self, pos):
+        pass
+
+    def _ownedByPlayer(self, player):
+        pass
+
+if __name__ == '__main__':
+    l = Lobby()
+    l.waitForPlayers()
+
+
+
